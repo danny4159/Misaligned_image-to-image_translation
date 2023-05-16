7713ed8ab7f1fe8b5f583b338302787f3cf2419a
Daniel <danny4159@naver.com>
2023-05-16 01:14:37 +0000

    feat: Add psnr,ssim metric and fix image scale

--- util/util.py
+++ util/util.py
@@ -14,6 +14,22 @@ def tensor2im(image_tensor, imtype=np.uint8):
     image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0 # scale: -1~1 -> 0~255
     return image_numpy.astype(imtype)
 
+def tensor2im_minmax(image_tensor, imtype=np.uint8):
+    image_numpy = image_tensor[0].cpu().float().numpy()
+    if image_numpy.shape[0] == 1:
+        image_numpy = np.tile(image_numpy, (3, 1, 1))
+        
+    # Find min and max values
+    min_val = np.min(image_numpy)
+    max_val = np.max(image_numpy)
+
+    # Scale to 0~1 range
+    image_numpy = (image_numpy - min_val) / (max_val - min_val)
+
+    # Scale to 0~255
+    image_numpy = np.transpose(image_numpy, (1, 2, 0)) * 255.0 
+    return image_numpy.astype(imtype)
+
 
 def diagnose_network(net, name='network'):
     mean = 0.0
